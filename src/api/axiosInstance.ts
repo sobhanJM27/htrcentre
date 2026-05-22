3fc77df8ae886ef3f426e91c59e3f0901cc9d536
sobhanJM27 <sobhanj27@gmail.com>
2026-05-22 03:48:38 +0330

    env production and development removed 2026-22-5

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const BASE_URL = "https://retrofit-nxb4.onrender.com/";
-export const PUBLIC_BASE_URL = import.meta.env.VITE_PUBLIC_BASE_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
